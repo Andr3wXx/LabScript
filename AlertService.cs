5dae786408d58d00fc2981ffabcbf9f58714274c
Richard Andrew Alvarez Dorado <radorado@grupodecor.local>
2024-12-07 15:40:18 -0500

    Configuracion de IAlertService

--- AlertService.cs
+++ AlertService.cs
@@ -1,6 +1,36 @@
-﻿namespace LabScript
+﻿
+using Microsoft.JSInterop;
+using System.Diagnostics.Contracts;
+
+namespace LabScript
 {
-    public class AlertService
+    public class AlertService : IAsyncDisposable, IAlertService
     {
+        readonly Lazy<Task<IJSObjectReference>> ijsObjectReference;
+
+        public AlertService(IJSRuntime ijsRuntime)
+        {
+            this.ijsObjectReference = new Lazy<Task<IJSObjectReference>>(() =>
+            ijsRuntime.InvokeAsync<IJSObjectReference>("import",
+            "./content/LabScript/Pages/Home.razor.js").AsTask());
+
+
+        }
+        public async ValueTask DisposeAsync()
+        {
+            if (ijsObjectReference.IsValueCreated)
+            {
+                IJSObjectReference moduleJs = await ijsObjectReference.Value;
+                await moduleJs.DisposeAsync();
+            }
+        }
+
+        public async Task CallJsFuction()
+        {
+            var jsModule = await ijsObjectReference.Value;
+
+            await jsModule.InvokeVoidAsync("jsFuncion");
+
+        }
     }
 }
---
+++ Pages/Home.js
@@ -0,0 +1,3 @@
+﻿export fuction jsFucion(){
+    alert('Prueba');
+}
